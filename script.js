5a6c9e4e5bdd26e169bec99312d2bcaab4630e03
IagoLeal <Iagomleal@hotmail.com>
2023-03-31 15:15:18 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-nexts',
+    prevEl: '.swiper-button-prevs',
+  },
+
+});
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-nexts',
+    prevEl: '.swiper-button-prevs',
+  },
+
+});
